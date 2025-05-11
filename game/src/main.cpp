74ef603621b0287d30192ffd2e5729aea3315285
Bogdan Mocanu <bogdan.game.development@gmail.com>
2025-05-11 17:18:24 +0200

    cleanup

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,14 +1,15 @@
 #include "engine.hpp"
+#include "fmt/format.h"
+#include "utility/console.hpp"
 
-int main() {
-
-  tale::WriteLine("Hello Game!");
-
-
+int main()
+{
   auto& engineInstance = tale::Engine::Instance();
   engineInstance.Init();
+  tale::Engine another;
+  tale::log::Info("{}", fmt::ptr(&engineInstance));
   engineInstance.Run();
   engineInstance.Quit();
 
-
+  another;
 }
